bff70b293efb33fc55f1ce32d24ed62132217542
hong <hong@xiyanghui.com>
2016-12-26 11:10:18 +0800

    Add form login

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -24,7 +25,7 @@ class DefaultController extends Controller
      */
     public function adminAction()
     {
-        return new Response('<html><body>Admin page!</body></html>');
+        return new Response('<html><body>Welcome to Admin page!</body></html>');
     }
 
 }
